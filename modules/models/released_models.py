1030528cb46d3f573f5c132347f2f1b63fbb080a
king-menin <login-const@mail.ru>
2019-04-10 14:21:50 +0300

    fix released

--- modules/models/released_models.py
+++ modules/models/released_models.py
@@ -1,10 +1,18 @@
-# from modules.layers.encoders import *
-# from modules.layers.decoders import *
+from .encoders import *
+from .decoders import *
 
 
 released_models = {
     "BertBiLSTMNCRF": {
         "encoder": BertBiLSTMEncoder,
+        "decoder": NCRFDecoder
+    },
+    "BertBiLSTMAttnNCRFJoint": {
+        "encoder": BertBiLSTMEncoder,
+        "decoder": AttnNCRFJointDecoder
+    },
+    "BertBiLSTMAttnNCRF": {
+        "encoder": AttnNCRFDecoder,
         "decoder": NCRFDecoder
     }
 }
